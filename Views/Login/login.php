5083c154aafbf424910f4c19014a5cfaafdf2604
jo1987 <lukeba_jocelyne@outlook.com>
2019-09-04 16:18:22 +0200

    ajout formulaire inscription

---
+++ Views/Login/login.php
@@ -0,0 +1,7 @@
+<form action=""></form>
+<label for="nom">Nom :</label><input type="text" id="nom" name="nom" value=""><br>
+<label for="prenom">Prénom:</label><input type="text"id="Prenom" name="prenom" =""><br>
+<label for="email">Email:</label> <input type="text" id="email" name="email"><br>
+<label for="text">Telephone:</label><input type="text" id="telephone" name="telephone"><br>
+<label for="text">Pays:</label> 
+<select name="Pays" id="pays"></select>
