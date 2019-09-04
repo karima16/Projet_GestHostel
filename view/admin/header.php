5f60a3d3a04b2db6f0cdb58c4d230561969eaa7e
HESS Gregory <hessgregory@hotmail.com>
2019-09-04 16:29:40 +0200

    Debut de création du header. Voir et reflechir comment organiser le modele

---
+++ view/admin/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Interface Admin</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
+<body>
+
+<header>
+	<div class="jumbostron">
+		<div class="logo">
+		</div>
+		<div class="titleLogo">
+		</div>
+	</div>
+	<nav>
+		<ul>
+			<li>test</li>
+		</ul>
+	</nav>
+</header>
