6d5a7569dccfe165f757f84a13be0bbd55e7c81f
Fabio <fabio.lopes@hotmail.be>
2019-09-04 16:05:43 +0200

    Ajout du style.css

--- Views/Pages/header.php
+++ Views/Pages/header.php
@@ -4,5 +4,6 @@
 	<meta charset="UTF-8">
 	<title>GestHostel</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="Contents/style.css">
 </head>
 <body>
