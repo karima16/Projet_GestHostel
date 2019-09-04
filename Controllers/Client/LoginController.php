53a41f7e109d4d5e0b738c31cd9f7746bcc93562
Mathieu S <mathieu.steinbach@live.be>
2019-09-04 16:35:05 +0200

    Rajout login pour la section et requete ajax pour les pays

--- Controllers/Client/LoginController.php
+++ Controllers/Client/LoginController.php
@@ -1,3 +1,21 @@
 <?php 
+
 require_once("Views/Login/login.php");
- ?>
+
+?>
+
+<script>
+let uri = "https://restcountries.eu/rest/v2/all";
+$.ajax({
+    url : uri,
+	type : 'GET',
+	dataType : 'html',
+    success:function(rep, statut){
+        let objects = eval('('+rep+')');
+        console.log(objects);
+        for (let i = 0; i < objects.length; i++) {
+            $("#pays").append("<option value="+objects[i].name+">"+objects[i].name+"</option>");
+        }
+    }
+});
+</script>
