c328aba08ace2d9eb7570d11f986bc046eb699c7
Mathieu S <mathieu.steinbach@live.be>
2019-09-06 09:06:51 +0200

    Script deplacé vers la vue

--- Views/Login/login.php
+++ Views/Login/login.php
@@ -5,3 +5,19 @@
 <label for="text">Telephone:</label><input type="text" id="telephone" name="telephone"><br>
 <label for="text">Pays:</label> 
 <select name="Pays" id="pays"></select>
+
+<script>
+let uri = "https://restcountries.eu/rest/v2/all";
+$.ajax({
+    url : uri,
+	type : 'GET',
+	dataType : 'html',
+    success:function(rep, statut){
+        let objects = eval('('+rep+')');
+        console.log(objects);
+        for (let i = 0; i < objects.length; i++) {
+            $("#pays").append("<option value="+objects[i].name+">"+objects[i].name+"</option>");
+        }
+    }
+});
+</script>
