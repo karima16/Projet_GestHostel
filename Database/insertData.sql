6a966c1d78784058f186334213f38e4678228ade
klo <clochette1236@gmail.com>
2019-09-06 14:16:30 +0200

    data pictures room

--- Database/insertData.sql
+++ Database/insertData.sql
@@ -13,10 +13,40 @@ VALUES
 INSERT INTO `message` 
 (`messageContent`,`postDate`,`messageId`,`userId`,`subjectId`,`isActive`) 
 VALUES 
-("Lorem ipsum dolor","21/10/18",1,1,1,"1"),("Lorem ipsum dolor sit amet, consectetuer","28/09/19",2,2,2,"1"),
+("Lorem ipsum dolor","21/10/18",1,1,1,"1"),
+("Lorem ipsum dolor sit amet, consectetuer","28/09/19",2,2,2,"1"),
 ("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.","10/03/20",3,3,3,"1"),
 ("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed","17/03/19",4,4,4,"1"),
 ("Lorem","02/12/18",5,5,5,"1"),("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur","05/05/20",6,6,6,"1"),
 ("Lorem ipsum dolor sit amet,","21/08/19",7,7,7,"1"),("Lorem ipsum","21/06/20",8,8,8,"1"),
 ("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.","08/10/18",9,9,9,"0"),
 ("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.","06/01/20",10,10,10,"0");
+
+
+INSERT INTO `picture` (`pictureId`,`roomId`,`picturePath`) 
+VALUES 
+(1,1,"https://images.pexels.com/photos/1838554/pexels-photo-1838554.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(2,1,"https://images.pexels.com/photos/275845/pexels-photo-275845.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(3,2,"https://images.pexels.com/photos/2507010/pexels-photo-2507010.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(4,2,"https://images.pexels.com/photos/2467285/pexels-photo-2467285.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(5,2,"https://images.pexels.com/photos/262048/pexels-photo-262048.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(6,2,"https://images.pexels.com/photos/2736388/pexels-photo-2736388.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(7,3,"https://images.pexels.com/photos/271619/pexels-photo-271619.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(8,3,"https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(9,4,"https://images.pexels.com/photos/2029719/pexels-photo-2029719.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(10,5,"https://images.pexels.com/photos/210604/pexels-photo-210604.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(11,5,"https://images.pexels.com/photos/2507016/pexels-photo-2507016.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(12,6,"https://images.pexels.com/photos/2775320/pexels-photo-2775320.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(13,7,"https://images.pexels.com/photos/237393/pexels-photo-237393.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(14,7,"https://images.pexels.com/photos/2725675/pexels-photo-2725675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(15,8,"https://images.pexels.com/photos/210604/pexels-photo-210604.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(16,8,"https://images.pexels.com/photos/2290753/pexels-photo-2290753.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(17,8,"https://images.pexels.com/photos/1457847/pexels-photo-1457847.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(18,9,"https://images.pexels.com/photos/2134224/pexels-photo-2134224.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(19,9,"https://images.pexels.com/photos/1028379/pexels-photo-1028379.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(20,9,"https://images.pexels.com/photos/280121/pexels-photo-280121.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(21,9,"https://images.pexels.com/photos/189293/pexels-photo-189293.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(22,10,"https://images.pexels.com/photos/545012/pexels-photo-545012.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(23,10,"https://images.pexels.com/photos/279652/pexels-photo-279652.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(24,10,"https://images.pexels.com/photos/1030834/pexels-photo-1030834.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+(25,10,"https://images.pexels.com/photos/221457/pexels-photo-221457.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
