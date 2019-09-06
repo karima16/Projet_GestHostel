633b4dd1dd66dbf5affcc80a915fb65223e401b0
Sterio <sterio.zonios@hotmail.com>
2019-09-06 15:53:56 +0200

    ajout insert data subject

---
+++ Database/subject.sql
@@ -0,0 +1,13 @@
+INSERT INTO `subject` (`subjectId`,`subjectTitle`,`subjectContent`,`isActive`,`postDate`,`forumId`,`userId`) 
+VALUES 
+(1,"test 1", "content 1", "1", "11/12/16",1 ,1),
+(2,"test 2", "content 2", "2", "11/12/16",2 ,2),
+(3,"test 3", "content 3", "3", "11/12/16",3 ,3),
+(4,"test 4", "content 4", "4", "11/12/16",4 ,4),
+(5,"test 5", "content 5", "5", "11/12/16",5 ,5),
+(6,"test 6", "content 6", "6", "11/12/16",6 ,6),
+(7,"test 7", "content 7", "7", "11/12/16",7 ,7),
+(8,"test 8", "content 8", "8", "11/12/16",8 ,8),
+(9,"test 9", "content 9", "9", "11/12/16",9 ,9),
+(10,"test 10", "content 10", "10", "11/12/16",10 ,10)
+;
