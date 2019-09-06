b979966ceb230b00b6acb1a931e4a4a58d72b149
HESS Gregory <hessgregory@hotmail.com>
2019-09-06 11:08:06 +0200

    Correction fichier Greg

---
+++ Views/Admin/header.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Projet Labo">
+  <meta name="author" content="Evoliris Full STack Dev">
+
+  <title>GestHotel Admin</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <!-- Page level plugin CSS-->
+  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+<nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+  <a class="navbar-brand mr-1" href="index.html">GestHotel Admin</a>
+  <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+    <i class="fas fa-bars"></i>
+  </button>
+</nav>
+
+<div id="wrapper">
+
+    <!-- Sidebar -->
+      <ul class="sidebar navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Tableau de bord</span>
+          </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Hotels/Chambres</span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <h6 class="dropdown-header">Hotels:</h6>
+            <a class="dropdown-item" href="blank.html">Voir/modifier</a>
+            <a class="dropdown-item" href="blank.html">Ajouter</a>
+            <div class="dropdown-divider"></div>
+            <h6 class="dropdown-header">Chambres:</h6>
+            <a class="dropdown-item" href="blank.html">Voir/modifier</a>
+            <a class="dropdown-item" href="blank.html">Ajouter</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Clients</span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <h6 class="dropdown-header">Premier sous-menu :</h6>
+            <a class="dropdown-item" href="blank.html">Page 1</a>
+            <a class="dropdown-item" href="blank.html">Page 2</a>
+            <a class="dropdown-item" href="blank.html">Page 3</a>
+            <div class="dropdown-divider"></div>
+            <h6 class="dropdown-header">Deuxième sous-menu :</h6>
+            <a class="dropdown-item" href="blank.html">Page 4</a>
+            <a class="dropdown-item" href="blank.html">Page 5</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Reservations</span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <h6 class="dropdown-header">Premier sous-menu :</h6>
+            <a class="dropdown-item" href="blank.html">Page 1</a>
+            <a class="dropdown-item" href="blank.html">Page 2</a>
+            <a class="dropdown-item" href="blank.html">Page 3</a>
+            <div class="dropdown-divider"></div>
+            <h6 class="dropdown-header">Deuxième sous-menu :</h6>
+            <a class="dropdown-item" href="blank.html">Page 4</a>
+            <a class="dropdown-item" href="blank.html">Page 5</a>
+          </div>
+        </li>
+      
+        <div class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>FAQ</span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <h6 class="dropdown-header">Premier sous-menu :</h6>
+            <a class="dropdown-item" href="blank.html">Page 1</a>
+            <a class="dropdown-item" href="blank.html">Page 2</a>
+            <a class="dropdown-item" href="blank.html">Page 3</a>
+            <div class="dropdown-divider"></div>
+            <h6 class="dropdown-header">Deuxième sous-menu :</h6>
+            <a class="dropdown-item" href="blank.html">Page 4</a>
+            <a class="dropdown-item" href="blank.html">Page 5</a>
+          </div>
+        </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Forum</span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <h6 class="dropdown-header">Premier sous-menu :</h6>
+            <a class="dropdown-item" href="blank.html">Page 1</a>
+            <a class="dropdown-item" href="blank.html">Page 2</a>
+            <a class="dropdown-item" href="blank.html">Page 3</a>
+            <div class="dropdown-divider"></div>
+            <h6 class="dropdown-header">Deuxième sous-menu :</h6>
+            <a class="dropdown-item" href="blank.html">Page 4</a>
+            <a class="dropdown-item" href="blank.html">Page 5</a>
+          </div>
+        </li>
+      </ul>
