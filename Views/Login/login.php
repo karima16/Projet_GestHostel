b17391b0a0ae67d006a5fcfb2709aa14293a878a
Mathieu S <mathieu.steinbach@live.be>
2019-09-06 09:35:03 +0200

    debut style css login

--- Views/Login/login.php
+++ Views/Login/login.php
@@ -1,10 +1,26 @@
-<form action=""></form>
-<label for="nom">Nom :</label><input type="text" id="nom" name="nom" value=""><br>
-<label for="prenom">Prénom:</label><input type="text"id="Prenom" name="prenom" =""><br>
-<label for="email">Email:</label> <input type="text" id="email" name="email"><br>
-<label for="text">Telephone:</label><input type="text" id="telephone" name="telephone"><br>
-<label for="text">Pays:</label> 
-<select name="Pays" id="pays"></select>
+<div class="containerLogin">
+
+    <form action="">
+    <h2>Inscrivez-Vous</h2>
+    <label for="nom">Nom :</label><input type="text" id="nom" name="nom"><br>
+    <label for="prenom">Prénom:</label><input type="text"id="prenom" name="prenom"><br>
+    <label for="email">Email:</label> <input type="text" id="email" name="email"><br>
+    <label for="telephone">Telephone:</label><input type="text" id="telephone" name="telephone"><br>
+    <label for="pays">Pays:</label> 
+    <select name="pays" id="pays"></select>
+    <br>
+    <input type="submit" value ="Confirmer">
+    </form>
+
+    <form action="">
+    <h2>Connectez-Vous</h2>
+    <label for="login">Login :</label><input type="text" id="login" name="login"><br>
+    <label for="password">Password:</label><input type="password"id="password" name="password"><br>
+    <br>
+    <input type="submit" value ="Confirmer">
+    </form>
+
+</div>
 
 <script>
 let uri = "https://restcountries.eu/rest/v2/all";
