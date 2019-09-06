f16836f2640b196b4d20d1b4adfc3b6d2a93dff5
Myrddin000 <m.theunis12@gmail.com>
2019-09-06 12:31:16 +0200

    navbar +- fini

--- Views/Pages/navigation.php
+++ Views/Pages/navigation.php
@@ -3,15 +3,40 @@
 <nav id="navigation">
 	
 	<ul>
-		<li>Accueil</li>
-		<li>Forum</li>
-		<li>FAQ</li>
-		<li>Contact</li>
-		<li class="separation">Login</li>
-		<li>Inscription</li>
+		<li class="navigation"><a href="">Accueil</a></li>
+		<li class="navigation"><a href="">Forum</a></li>
+		<li class="navigation"><a href="">FAQ</a></li>
+		<li class="navigation"><a href="">Contact</a></li>
+		<li class="login navigation" ><a href="#">Login</a></li>
+		<li class="login navigation"><a href="#">Inscription</a></li>
+		<li id="profil">
+			<details>
+			    <summary>Profil Name</summary>
+			    <li><a href="">Profil User</a></li>
+				<li><a href="">Historique</a></li>
+				<li><a href="">Admin</a></li>
+				<li><a href="">disconnection</a></li>
+			</details>
+		</li>
 		
 	</ul>
 
 
 </nav>
 
+<script>
+	$(function(){
+		$('.login').click(function(){
+
+			if ($('#profil').css('display') == 'none') {
+				$('#profil').css('display','block');
+				$('.login').css('display','none')
+			}
+			else{
+				$('#profil').css('display','none');
+			}
+
+		})
+	})
+
+</script>
