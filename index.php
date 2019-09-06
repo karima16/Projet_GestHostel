2d400470b50f8eee5b04733a0853b1918fd1dc03
Myrddin000 <m.theunis12@gmail.com>
2019-09-06 13:58:43 +0200

    Switch/Case de direction vers pages

--- index.php
+++ index.php
@@ -1,11 +1,32 @@
 <?php 
 session_start();
 require_once("Views/Pages/header.php");
-// require_once("view/menu/menu.php");
+require_once("Controllers/navigation.php");
 if(isset($_GET["section"])){
-	// switch ($_GET["section"]) {
+	switch ($_GET["section"]) {
+		case 'accueil': 
+			require_once("Controllers/Client/HomeController.php");
+			break;
+		case 'forum':
+			break;
+		case 'faq':
+			break;
+		case 'contact':
+			break;
+		case 'login':
+			break;
+		case 'inscription':
+			break;
+		case 'profil user':
+			break;
+		case 'historique':
+			break;
+		case 'admin':
+			break;
+		case 'deconnexion':
+			break;
 		
-	// }
+	}
 }
 else {
 	require_once("Controllers/Client/rechercheAvancer.html");
