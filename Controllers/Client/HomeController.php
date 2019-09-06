f7bbdfd47de0f13307fab0aab10b0b5dc038ba2d
fabio <fabio.lopes@hotmail.be>
2019-09-06 11:40:40 +0200

    ajout fonction de montrer et cacher la recherche avancée

--- Controllers/Client/HomeController.php
+++ Controllers/Client/HomeController.php
@@ -17,4 +17,14 @@ require_once("Views/Home/home.php");
 	// Set the date
 	field.value = date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString().padStart(2, 0) + 
 	    '-' + date.getDate().toString().padStart(2, 0);
+
+	$('#btnRechercheAvancee').on('click',function(){
+		if (($('#rechercheAvancee').css('display')=='none')) {
+			$('#rechercheAvancee').css('display','block');
+		}
+		else{
+			$('#rechercheAvancee').css('display','none');
+		}
+
+	})
  </script>
