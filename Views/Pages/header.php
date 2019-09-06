392efbba1646c76cf5cbe16b4fb56974ea4d3758
fabio <fabio.lopes@hotmail.be>
2019-09-06 10:44:44 +0200

    ajout de script pour gérer l'input date spécial

--- Views/Pages/header.php
+++ Views/Pages/header.php
@@ -4,6 +4,10 @@
 	<meta charset="UTF-8">
 	<title>GestHostel</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+	<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+	<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 	<link rel="stylesheet" href="Contents/style.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 </head>
 <body>
