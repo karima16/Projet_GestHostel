89f3ab83886688bae29ebf4fe6e79abb9425f6ed
Myrddin000 <m.theunis12@gmail.com>
2019-09-06 13:58:06 +0200

    Ajout liens vers différentes sections

--- Views/Pages/navigation.php
+++ Views/Pages/navigation.php
@@ -3,19 +3,19 @@
 <nav id="navigation">
 	
 	<ul>
-		<li class="navigation"><a href="">Accueil</a></li>
-		<li class="navigation"><a href="">Forum</a></li>
-		<li class="navigation"><a href="">FAQ</a></li>
-		<li class="navigation"><a href="">Contact</a></li>
-		<li class="login navigation" ><a href="#">Login</a></li>
-		<li class="login navigation"><a href="#">Inscription</a></li>
+		<li class="navigation"><a href="?section=accueil">Accueil</a></li>
+		<li class="navigation"><a href="?section=forum">Forum</a></li>
+		<li class="navigation"><a href="?section=faq">FAQ</a></li>
+		<li class="navigation"><a href="?section=contact">Contact</a></li>
+		<li class="login navigation" ><a href="?section=login">Login</a></li>
+		<li class="login navigation"><a href="?section=inscription">Inscription</a></li>
 		<li id="profil">
 			<details>
 			    <summary>Profil Name</summary>
-			    <li><a href="">Profil User</a></li>
-				<li><a href="">Historique</a></li>
-				<li><a href="">Admin</a></li>
-				<li><a href="">disconnection</a></li>
+			    <li><a href="?section=profil">Profil User</a></li>
+				<li><a href="?section=historique">Historique</a></li>
+				<li><a href="?section=admin">Admin</a></li>
+				<li><a href="?section=deconnexion">Déconnexion</a></li>
 			</details>
 		</li>
 		
