387ae29118d9e2ef9bf5fa36f8f9401f0a839e7e
Myrddin000 <m.theunis12@gmail.com>
2019-09-06 09:09:22 +0200

    Start

---
+++ Views/Pages/navigation.php
@@ -0,0 +1,17 @@
+
+
+<nav id="navigation">
+	
+	<ul>
+		<li>Accueil</li>
+		<li>Forum</li>
+		<li>FAQ</li>
+		<li>Contact</li>
+		<li class="separation">Login</li>
+		<li>Inscription</li>
+		
+	</ul>
+
+
+</nav>
+
